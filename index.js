359b2829c124ea7fa36f43d34afa1cd263ffedb3
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 17:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-username = document.getElementById("username").value
-
 function getRepositories() {
+  username = document.getElementById("username").value
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayRepositories)
   req.open("GET", `https://api.github.com/users/${username}/repos`)
