fdfb095fde11b5c5e79d4cc732b2f3652a0dd7bb
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 17:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories() {
-  username = document.getElementById("username").value
+  const username = document.getElementById("username").value
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayRepositories)
   req.open("GET", `https://api.github.com/users/${username}/repos`)
@@ -15,7 +15,7 @@ function displayRepositories() {
 
 function getCommits(el) {
   const repoName = el.dataset.repo
-  console.log(el.dataset.username)
+  const username = document.getElementById("username").value
 }
 
 function displayCommits () {
