78794752e0c657922b7f9e4563bc6cffb0060735
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 17:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ function getRepositories() {
 }
 
 function displayRepositories() {
-  console.log("hit")
   var repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="'
    + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
-function getCommits() {
-
+function getCommits(el) {
+  const repoName = el.dataset.repository
 }
 
 function displayCommits () {
