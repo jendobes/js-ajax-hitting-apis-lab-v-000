ee4c8febfa77898234e586c72aee4c265c118fd1
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 17:31:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function displayRepositories() {
 function getCommits(el) {
   const repoName = el.dataset.repo
   const username = document.getElementById("username").value
+  console.log(username)
 }
 
 function displayCommits () {
