b6cbe3618e6387f048e4ff86e0354c15ced1ec43
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 18:00:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,8 @@ function getCommits(el) {
 
 function displayCommits () {
   const commits = JSON.parse(this.responseText)
-  const commitList = `<ul>${commits.map(commit => '<li><h3>'+ commit.commit.author.name +' ('+ commit.author.login +')</h3>'
-   + commit.commit.message' + </li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = commitList
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
 }
 
 function getBranches() {
