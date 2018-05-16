4b125c02e66f2afb2ca6db7fc5bc71b6a1cd4a44
Jennifer Dobak <jdobak1@gmail.com>
2018-05-16 18:26:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const rootURL = "https://api.github.com"
+const rootURL = "https://api.github.com"
 //
 function getRepositories() {
   const username = document.getElementById("username").value
@@ -7,40 +7,7 @@ function getRepositories() {
   req.open("GET", `https://api.github.com/users/${username}/repos`)
   req.send()
 }
-//
-function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="'
-   + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
-//
-// function getCommits(el) {
-//   const repoName = el.dataset.repo
-//   const username = document.getElementById("username").value
-//   const uri = `https://api.github.com/repos/${username}/${repoName}/commits`
-//   const xhr = new XMLHttpRequest()
-//   xhr.addEventListener("load", displayCommits)
-//   xhr.open("GET", uri)
-//   xhr.send()
-// }
-//
-// function displayCommits () {
-//   const commits = JSON.parse(this.responseText)
-//   const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
-//   ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
-//   document.getElementById("details").innerHTML = commitsList
-// }
-//
-// function getBranches() {
-//
-// }
-//
-// function displayBranches() {
-//
-// }
 
-const rootURL = "https://api.github.com"
 
 function displayRepositories() {
   const repos = JSON.parse(this.responseText)
